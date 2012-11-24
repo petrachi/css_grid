24dc42e3721bf5550c5aaf378248affee8764846
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2012-11-24 02:51:04 +0100

    css grid - adding rake task

--- lib/css_grid.rb
+++ lib/css_grid.rb
@@ -1,8 +1,9 @@
 require "css_grid/version"
 require "css_grid/engine"
-require 'css_grid/railtie' if defined?(Rails)
 
 module GridHelper
+  require 'css_grid/railtie' if defined?(Rails)
+  
   TWELVE_STRING_INTS = { :one => 1, :two => 2, :three => 3, :four => 4, :five => 5, :six => 6, 
                          :seven => 7, :eight => 8, :nine => 9, :ten => 10, :eleven => 11, :twelve => 12 }
   TWELVE_STRING_INTS_INVERT = TWELVE_STRING_INTS.invert
