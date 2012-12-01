221613df8a948cb7ac519916a1ffba7db0415357
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2012-12-01 06:36:03 +0100

    css grid - bugfix in_groups_of

--- lib/css_grid.rb
+++ lib/css_grid.rb
@@ -80,7 +80,8 @@ module GridHelper
     collection_length = TWELVE_STRING_INTS[col_number]
     span_width = @span_width || TWELVE_STRING_INTS_INVERT[(options.delete(:nested_width) || 12) / (collection_length + (options[:spans][:prepend] || 0) + (options[:spans][:append] || 0))]
     
-    rows = (options.delete(:collection) || [1]).in_groups_of(collection_length, false) do |collection_mini|
+    rows = []
+    (options.delete(:collection) || [1]).in_groups_of(collection_length, false) do |collection_mini|
       cols = collection_mini.map do |elt|
         @elt = elt
         
@@ -98,7 +99,7 @@ module GridHelper
       end
       @elt = nil
 
-      grid(:row, options[:rows].clone){ cols.reduce(:safe_concat) }
+      rows << grid(:row, options[:rows].clone){ cols.reduce(:safe_concat) }
     end
     
     safe_buffer = rows.reduce(:safe_concat)
